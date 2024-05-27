fc80b20a6fbe34631183d4c46c8a2e13ea1962fa
muhammed aslam <aslamhref@gmail.com>
2024-05-27 11:18:40 +0530

    startedapp blog,comments,tags,users

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&r$l@fs-8-l79qz#-4=fwgq%a-spd@_m*hl1u3+b3o53wgxaei'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'decouple',
+    'blog',
+    'comments',
+    'tags',
+    'users'
 ]
 
 MIDDLEWARE = [
