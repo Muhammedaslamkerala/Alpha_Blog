dabdae2089943f927ee037648716e395869b6814
muhammed aslam <aslamhref@gmail.com>
2024-05-28 18:30:31 +0530

    Defined models for , , , ,

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
+from typing import Any
 from django.db import models
-
+from users.models import Author
+from tags.models import Tag
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50,unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, related_name='c_posts')
+    tags = models.ManyToManyField(Tag, related_name='t_posts')
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from blog.models import Post
+from users.models import Author
 # Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    body = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.author.user.username} - {self.post.title}'
